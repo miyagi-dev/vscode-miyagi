e7c8d354e01924d2fe50b1f7769c9ab13789cd70
Fynn Becker <post@fynn.be>
2022-08-22 11:24:14 +0200

    refactor: Move global types into separate file

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,23 @@
+import { MiyagiConfig } from './types'
+
 export const MIYAGI_CONFIG_GLOB = '**/.miyagi.{js,json}'
 export const SCHEMA_GLOB = '**/schema.{json,yaml}'
 export const MOCKS_GLOB = '**/mocks.{json,yaml}'
 export const TWIG_GLOB = '**/*.twig'
 export const EXCLUDE_GLOB = '**/node_modules/**'
+
+export const DEFAULT_MIYAGI_CONFIG: MiyagiConfig = {
+	components: {
+		folder: 'src'
+	},
+	files: {
+		schema: {
+			name: 'schema',
+			extension: 'json'
+		},
+		mocks: {
+			name: 'mocks',
+			extension: 'json'
+		}
+	}
+}
